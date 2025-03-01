d9fab5f0dbaacb2948b4b086645a52e57c66365c
Anshul Singh <anshulsingh55@yahoo.com>
2025-03-01 19:50:52 +0530

    Added Eureka Client

--- orderService/orderService/src/main/java/com/ecommerce/orderService/OrderServiceApplication.java
+++ orderService/orderService/src/main/java/com/ecommerce/orderService/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.ecommerce.orderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
